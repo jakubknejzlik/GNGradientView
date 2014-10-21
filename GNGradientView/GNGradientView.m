f2e2fdbe427b818c233aa845cc85962d105cfaa7
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2014-10-21 04:21:57 +0200

    test removal

--- GNGradientView/GNGradientView.m
+++ GNGradientView/GNGradientView.m
@@ -13,20 +13,11 @@
 -(id)initWithCoder:(NSCoder *)aDecoder{
     self = [super initWithCoder:aDecoder];
     if (self) {
-        [self setGradientMaskAlphaChannels:@[@1,@0.8,@0]];
+        [self setGradientMaskAlphaChannels:@[@1,@0]];
         [self updateGradientMaskFrame];
         [self setGradientMaskDirection:GNGradientViewDirectionBottom];
-        
-        [NSTimer scheduledTimerWithTimeInterval:0.8 target:self selector:@selector(updateTest) userInfo:nil repeats:YES];
     }
     return self;
-}
-
-
-
--(void)updateTest{
-    self.frame = CGRectOffset(CGRectInset(self.frame, 0, 1),0,-1);
-    [self updateGradientMaskFrame];
 }
 
 @end
